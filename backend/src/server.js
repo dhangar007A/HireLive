5f5d315547d3acf9d3599281aa32096dfa00c5f1
dhangar007A <147794851+dhangar007A@users.noreply.github.com>
2025-12-07 11:02:45 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
+import {serve} from 'inngest/express';
+import { inngest } from './lib/inngest.js';
 
 
 const app = express();
 const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json());
+//Credentials=true means => server allows a browser to include cookies on requests
+app.use(cors({origin:ENV.CLIENT_URL}, Credentials=true));
+
+app.use('/inngest', serve({client: inngest, functions}));
 
 app.get("/health", (req, res) => {
     res.status(200).json({msg: "Hello, healthy!"});
